3e9dfb725ea548a717546c174cfe4ddcc8ebd7e7
ligofff <liga@ligofff.ru>
2023-11-15 15:35:43 +0300

    Package initial commit

---
+++ Editor/AssetShortcutEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AssetShortcut))]
+public class AssetShortcutEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AssetShortcut assetShortcut = (AssetShortcut)target;
+
+        var objField = typeof(AssetShortcut).GetField("obj", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        Object obj = EditorGUILayout.ObjectField("Object", (Object)objField.GetValue(assetShortcut), typeof(Object), true);
+        objField.SetValue(assetShortcut, obj);
+
+        EditorGUILayout.HelpBox("This is only a shortcut of asset placed in " + AssetDatabase.GetAssetPath(obj), MessageType.Warning);
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        if (obj != null)
+        {
+            Editor objEditor = CreateEditor(obj);
+            objEditor.OnInspectorGUI();
+        }
+    }
+}
---
+++ Runtime/AssetShortcut.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[CreateAssetMenu(fileName = "new Shortcut", menuName = "Ligofff/Create asset shortcut")]
+public class AssetShortcut : ScriptableObject
+{
+    [SerializeField]
+    private Object obj;
+    public Type Type => obj.GetType();
+    
+    public Object Get => obj;
+    public T GetSmart<T> () where T : Object => (T)obj;
+}
